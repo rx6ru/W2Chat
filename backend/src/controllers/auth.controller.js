06f5013aba6243654c927a8bd1f6c449ca9acc24
rxbru <amarmahato.cs@gmail.com>
2025-07-15 19:45:37 +0530

    Revert "once more with * fix"
    This reverts commit e82ab4ca3f2d1a195988a08cc87939bbb7637ac7.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,155 +8,190 @@ import validator from "validator";
 
 //todo: Protect against SQL injection
 
-export const signup = asyncHandler(async (req, res) => {
+export const signup = async (req, res) => {
     let { fullName, email, password, inviteCode } = req.body;
-    // Sanitize invite code and email
-    inviteCode = validator.escape(String(inviteCode || ""));
-    email = validator.normalizeEmail(String(email || ""));
-    fullName = validator.escape(String(fullName || ""));
+    try {
+        // Sanitize invite code and email
+        inviteCode = validator.escape(String(inviteCode || ""));
+        email = validator.normalizeEmail(String(email || ""));
+        fullName = validator.escape(String(fullName || ""));
 
-    if (!fullName || !email || !password || !inviteCode) {
-        return res.status(400).json({
-            message: "All fields and invite code are required",
+        if (!fullName || !email || !password || !inviteCode) {
+            return res.status(400).json({
+                message: "All fields and invite code are required",
+            });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({
+                message: "Password must be at least 6 characters",
+            });
+        }
+
+        //use zod validation
+        const result = userSchema.safeParse({ fullName, email, password });
+        if (!result.success) {
+            return res.status(400).json({
+                message: `Error-${result.error.issues[0].message}`,
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                message: "Email already exists",
+            });
+        }
+
+        // Check invite code in DB
+        const codeDoc = await InviteCode.findOne({ code: inviteCode });
+        if (!codeDoc) {
+            return res.status(400).json({
+                message: "Invalid invite code",
+            });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await new User({
+            fullName,
+            email,
+            password: hashedPassword,
+        });
+
+        if (!newUser) {
+            return res.status(400).json({
+                message: "User not created - Invalid User Data",
+            });
+        }
+
+        generateToken(newUser._id, res);
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            email: newUser.email,
+            profilePic: newUser.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in signup controller -", error.message);
+        return res.status(500).json({
+            message: "Internal Server Error",
         });
     }
+};
 
-    if (password.length < 6) {
-        return res.status(400).json({
-            message: "Password must be at least 6 characters",
-        });
-    }
-
-    //use zod validation
-    const result = userSchema.safeParse({ fullName, email, password });
-    if (!result.success) {
-        return res.status(400).json({
-            message: `Error-${result.error.issues[0].message}`,
-        });
-    }
-
-    const user = await User.findOne({ email });
-    if (user) {
-        return res.status(400).json({
-            message: "Email already exists",
-        });
-    }
-
-    // Check invite code in DB
-    const codeDoc = await InviteCode.findOne({ code: inviteCode });
-    if (!codeDoc) {
-        return res.status(400).json({
-            message: "Invalid invite code",
-        });
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const newUser = await new User({
-        fullName,
-        email,
-        password: hashedPassword,
-    });
-
-    if (!newUser) {
-        return res.status(400).json({
-            message: "User not created - Invalid User Data",
-        });
-    }
-
-    generateToken(newUser._id, res);
-    await newUser.save();
-
-    res.status(201).json({
-        _id: newUser._id,
-        fullName: newUser.fullName,
-        email: newUser.email,
-        profilePic: newUser.profilePic,
-    });
-});
-
-export const login = asyncHandler(async (req, res) => {
+export const login = async (req, res) => {
     const { email, password } = req.body;
 
-    if (!email || !password) {
-        return res.status(400).json({
-            message: "All fields are required",
+    try {
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "All fields are required",
+            });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({
+                message: "Password must be at least 6 characters",
+            });
+        }
+
+        const result = loginSchema.safeParse({ email, password });
+        if (!result.success) {
+            return res.status(400).json({
+                message: `Error-${result.error.issues[0].message}`,
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid credentials",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid credentials",
+            });
+        }
+
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in login controller -", error.message);
+        return res.status(500).json({
+            message: "Internal Server Error",
         });
     }
+};
 
-    if (password.length < 6) {
-        return res.status(400).json({
-            message: "Password must be at least 6 characters",
+export const logout = async (req, res) => {
+    try {
+        await res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({
+            message: "Logout successful",
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error",
         });
     }
+};
 
-    const result = loginSchema.safeParse({ email, password });
-    if (!result.success) {
-        return res.status(400).json({
-            message: `Error-${result.error.issues[0].message}`,
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId=req.user._id;
+
+        if(!profilePic) {
+            return res.status(400).json({
+                message: "Profile picture is required",
+            });
+        } 
+
+        const uploadResponse=await cloudinary.uploader.upload(profilePic)
+
+        if(!uploadResponse) {
+            return res.status(400).json({
+                message: "Profile picture upload failed",
+            });
+        }
+
+        const updatedUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
+
+        if(!updatedUser) {
+            return res.status(400).json({
+                message: "Profile picture update failed",
+            });
+        }
+
+        res.status(200).json(updatedUser);
+
+    } catch (error) {
+        console.log("Error in updateProfile controller -", error);
+        return res.status(500).json({
+            message: "Internal Server Error",
         });
     }
+}
 
-    const user = await User.findOne({ email });
-    if (!user) {
-        return res.status(400).json({
-            message: "Invalid credentials",
+export const checkAuth = async (req,res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller -", error.message);
+        res.status(500).json({
+            message: "Internal Server Error",
         });
     }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-        return res.status(400).json({
-            message: "Invalid credentials",
-        });
-    }
-
-    generateToken(user._id, res);
-    res.status(200).json({
-        _id: user._id,
-        fullName: user.fullName,
-        email: user.email,
-        profilePic: user.profilePic,
-    });
-});
-
-export const logout = asyncHandler(async (req, res) => {
-    await res.cookie("jwt", "", { maxAge: 0 });
-    res.status(200).json({
-        message: "Logout successful",
-    });
-});
-
-export const updateProfile = asyncHandler(async (req, res) => {
-    const {profilePic} = req.body;
-    const userId=req.user._id;
-
-    if(!profilePic) {
-        return res.status(400).json({
-            message: "Profile picture is required",
-        });
-    } 
-
-    const uploadResponse=await cloudinary.uploader.upload(profilePic)
-
-    if(!uploadResponse) {
-        return res.status(400).json({
-            message: "Profile picture upload failed",
-        });
-    }
-
-    const updatedUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
-
-    if(!updatedUser) {
-        return res.status(400).json({
-            message: "Profile picture update failed",
-        });
-    }
-
-    res.status(200).json(updatedUser);
-})
-
-export const checkAuth = asyncHandler(async (req,res) => {
-    res.status(200).json(req.user);
-})
+}
--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,52 +2,68 @@ import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 import cloudinary from "../lib/cloudinary.js";
 import { getReceiverSocketId, io} from "../lib/socket.js";
-import asyncHandler from "../lib/utils/asyncHandler.js";
 
-export const getUsersForSidebar = asyncHandler(async (req, res) => {
-    const loggedInUserId=req.user._id;
-    const filteredUsers=await User.find({_id:{$ne:loggedInUserId}}).select("-password");
-    res.status(200).json(filteredUsers);
-});
-
-export const getMessages = asyncHandler(async (req, res) => {
-    const receiverId=req.params.id;
-    const loggedInUserId=req.user._id;
-    const messages=await Message.find({
-        $or:[
-            {senderId:loggedInUserId,receiverId:receiverId},
-            {senderId:receiverId,receiverId:loggedInUserId}
-        ]
-    });
-
-    res.status(200).json(messages);
-});
-
-export const sendMessage = asyncHandler(async (req, res) => {
-    const {text,image}=req.body;
-    const senderId=req.user._id;
-    const receiverId=req.params.id;
-
-    let imageUrl;
-
-    if(image){
-        const uploadResponse=await cloudinary.uploader.upload(image);
-        imageUrl=uploadResponse.secure_url;
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        const loggedInUserId=req.user._id;
+        const filteredUsers=await User.find({_id:{$ne:loggedInUserId}}).select("-password");
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.log("Error in getUsersForSidebar controller",error.message);
+        res.status(500).json({message:"Internal Server Error"});
     }
+} 
 
-    const newMessage=await Message.create({
-        senderId,
-        receiverId,
-        text,
-        image:imageUrl
-    });
+export const getMessages = async (req, res) => {
+    try {
+        const receiverId=req.params.id;
+        const loggedInUserId=req.user._id;
+        const messages=await Message.find({
+            $or:[
+                {senderId:loggedInUserId,receiverId:receiverId},
+                {senderId:receiverId,receiverId:loggedInUserId}
+            ]
+        });
 
-    await newMessage.save();
-
-    const receiverSocketId=await getReceiverSocketId(receiverId);
-    if(receiverSocketId){
-        io.to(receiverSocketId).emit("newMessage",newMessage);
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in getMessages controller",error.message);
+        res.status(500).json({message:"Internal Server Error"});
     }
+}
 
-    res.status(201).json(newMessage);
-});
+export const sendMessage = async (req, res) => {
+    try {
+        const {text,image}=req.body;
+        const senderId=req.user._id;
+        const receiverId=req.params.id;
+
+        let imageUrl;
+
+        if(image){
+            const uploadResponse=await cloudinary.uploader.upload(image);
+            imageUrl=uploadResponse.secure_url;
+        }
+
+        const newMessage=await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        });
+
+        await newMessage.save();
+
+        const receiverSocketId=await getReceiverSocketId(receiverId);
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage",newMessage);
+        }
+
+        res.status(201).json(newMessage);
+
+    } catch (error) {
+        console.log("Error in sendMessage controller",error.message);
+        res.status(500).json({message:"Internal Server Error"});        
+    }
+     
+}
